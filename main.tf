af38992c5e403e8903c46fd7a2f4f4b6571750c5
KoulteghDevOps <koultegh@outlook.com>
2023-05-12 12:36:20 +0100

    secret syntax added

--- main.tf
+++ main.tf
@@ -1,5 +1,6 @@
 resource "aws_ssm_parameter" "parameter" {
   name  = "dev.test"
-  type  = "String"
+  type  = "Securestring"
   value = "Hello-World"
+  key_id = "c770d687-27ad-4188-b4b0-c3d4ce34a454"
 }
