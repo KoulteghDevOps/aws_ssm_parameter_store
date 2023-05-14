8c58543c8abd3d4ceaa25ea6a24fa0abced1ca26
KoulteghDevOps <koultegh@outlook.com>
2023-05-14 21:23:49 +0100

    snippet updated

--- main.tf
+++ main.tf
@@ -1,7 +1,8 @@
 resource "aws_ssm_parameter" "parameters" {
   count = length(var.parameters)
   name = var.parameters[count.index].name
-  type  = "SecureString"
+  type  = "String"
+  # type  = "SecureString"
   value = var.parameters[count.index].value
   key_id = "c770d687-27ad-4188-b4b0-c3d4ce34a454"
 }
