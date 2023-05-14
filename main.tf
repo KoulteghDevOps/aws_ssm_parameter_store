e345eb561fad244914fa0a84deea9ebbc4acb1a4
KoulteghDevOps <koultegh@outlook.com>
2023-05-14 20:31:16 +0100

    snippet updated

--- main.tf
+++ main.tf
@@ -1,6 +1,14 @@
-resource "aws_ssm_parameter" "parameter" {
-  name  = "dev.test"
+resource "aws_ssm_parameter" "parameters" {
+  count = length(var.parameters)
+  name = var.parameters[count.index].name
   type  = "SecureString"
-  value = "Hello-World"
+  value = var.parameters[count.index].value
   key_id = "c770d687-27ad-4188-b4b0-c3d4ce34a454"
 }
+
+# resource "aws_ssm_parameter" "parameters{
+#   name  = "dev.test"
+#   type  = "SecureString"
+#   value = "Hello-World"
+#   key_id = "c770d687-27ad-4188-b4b0-c3d4ce34a454"
+# }
